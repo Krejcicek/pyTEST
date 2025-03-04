05f24b1f3cf10439e1f79bd0217513bd91091ed2
Krejcicek <94050047+Krejcicek@users.noreply.github.com>
2025-03-04 10:27:51 +0100

    Add files via upload

---
+++ pytestes/test_calculator_parametrized.py
@@ -0,0 +1,26 @@
+import pytest
+from src import calculator
+
+def test_add_2():
+    assert calculator.add(1,2) == 3
+    assert calculator.add(0,5) == 5
+    assert calculator.add(-2,9) == 7
+    assert calculator.add(0,0) == 0
+
+def test_add_2():
+    assert calculator.add(1,2) == 3
+    assert calculator.add(0,5) == 5
+    assert calculator.add(-2,9) == 7
+    assert calculator.add(5,-8) == -3
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (1,2,3),
+        (0,5,5),
+        (-2,9,7),
+        (5,-8,-3),
+    ]
+)
+def test_add_parametrize(a, b, expected):
+    assert calculator.add(a,b) == expected
